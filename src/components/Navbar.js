22a409a57fb60332d9e2a8aec68997af7f2b5976
Popol <github.afiqqo@gmail.com>
2025-01-10 04:01:29 +0800

    navbar done

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,21 +5,28 @@ import { Link } from "react-router-dom"
 
 
 class Navbar extends Component {
+  state = {clicked:false};
+  handleClick = () =>{
+    this.setState({clicked: !this.state.clicked})
+  }
   render() {
     return (
       <nav className="NavbarItems">
         <h1 className="navbar-logo">Penang</h1>
-        <ul className="nav-menu">
+        <div className="menu-icon" onClick={this.handleClick}>
+          <i className={this.state.clicked ? "fas fa-times" : "fas fa-bars"}></i>          
+        </div>
+        <ul className={this.state.clicked ? "nav-menu active" : "nav-menu"}>
           {MenuItems.map((item, index) =>{
             return(
               <li key = {index}>
-                <a href={item.url}>
+                <a className={item.cName} href={item.url}>
                   <i className={item.icon}></i>{item.title}
                 </a>
               </li>
             )
           })}
-          
+          <button>Contact Us</button>
         </ul>
       </nav>
     );
