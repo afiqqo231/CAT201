9f07dd78be56b8e598ee1e5fa4ad5108a107fd71
Popol <github.afiqqo@gmail.com>
2025-01-10 20:35:49 +0800

    fix heading for About and Gallery page

--- src/routes/About.js
+++ src/routes/About.js
@@ -6,7 +6,12 @@ function About (){
     return (
         <>
         <Navbar/>
-        <Hero cName="hero-mid" heroImg={AboutImg} title="About Penang" desc="intro"/>
+        <Hero
+        cName="hero-mid"
+        heroImg={AboutImg}
+        title="About Penang"
+        desc="Penang[a] is a Malaysian state located on the northwest coast of Peninsular Malaysia along the Strait of Malacca. It has two parts: Penang Island, where the capital city, George Town, is located, and Seberang Perai on the Malay Peninsula. These two halves are physically connected by the Penang Bridge and the Second Penang Bridge. The state shares borders with Kedah to the north and east, and Perak to the south."
+        button="hide"/>
         </>
     )
 }
