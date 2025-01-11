b01c528bce9bc773d255e4b56b3e26de29a9f96f
Popol <github.afiqqo@gmail.com>
2025-01-12 03:14:15 +0800

    footer update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,17 +4,18 @@ function Footer(){
     return(
         <div className="footer">
             <h1>References</h1>
-            <a href="https://mypenang.gov.my/">
-                <p>MyPenang</p>
-            </a>
-            <a href="https://www.tripadvisor.com.my/Tourism-g660694-Penang_Island_Penang-Vacations.html">
-                <p>WikiTravel</p>
-            </a>
-            <a href="https://mypenang.gov.my/">
-                <p>TripAdvisor</p>
-            </a>
+            <div className="footer-content">
+                <a href="https://mypenang.gov.my/">
+                    <p>MyPenang</p>
+                </a>
+                <a href="https://www.tripadvisor.com.my/Tourism-g660694-Penang_Island_Penang-Vacations.html">
+                    <p>WikiTravel</p>
+                </a>
+                <a href="https://mypenang.gov.my/">
+                    <p>TripAdvisor</p>
+                </a>
+            </div>
         </div>
-
     )
 }
 
