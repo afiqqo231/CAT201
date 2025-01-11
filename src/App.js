6faf2ce4f381e36cf60a378f044ad79ac51792eb
Popol <github.afiqqo@gmail.com>
2025-01-12 00:06:25 +0800

    ntahlah

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { Route, Routes } from "react-router-dom"
 import Home from "./routes/Home.js";
 import Food from "./routes/Food.js";
 import Places from "./routes/Places.js";
-import Entertainment from "./routes/Entertainment.js";
 
 export default function App() {
   return (
@@ -12,7 +11,6 @@ export default function App() {
         <Route path="/CAT201" element = {<Home/>}/>
         <Route path="/CAT201/places" element = {<Places/>}/>
         <Route path="/CAT201/food" element = {<Food/>}/>
-        <Route path="/CAT201/entertainment" element = {<Entertainment/>}/>
       </Routes>
 
     </div>
